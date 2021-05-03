b371ad4bb7da205c17a42d878a02e8534e66990c
tejan-singh <tstejan1@gmail.com>
2021-05-03 22:55:03 +0530

    added blogs sections

---
+++ src/Components/Blogs.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Blogs(){
+
+    return (
+        <div>
+            <section className="blog-section" id="blogs">
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 blog-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="price-content">Blog Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="">Read More...</a></p>
+                                <p>Submitted by <a href="">Tejan Singh</a> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 blog-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="price-content">Blog Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="">Read More...</a></p>
+                                <p>Submitted by <a href="">Tejan Singh</a> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 blog-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="price-content">Blog Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="">Read More...</a></p>
+                                <p>Submitted by <a href="">Tejan Singh</a> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 blog-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="price-content">Blog Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="">Read More...</a></p>
+                                <p>Submitted by <a href="">Tejan Singh</a> </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Blogs;
